7fd048f401c188a2cc4ecf3d6b4719ad5eca1d76
ddalkigum <sol35352000@gmail.com>
2022-06-22 18:54:35 +0900

    ADD: Joi library, apply party router

--- src/domain/party/router.ts
+++ src/domain/party/router.ts
@@ -1,9 +1,11 @@
 import { NextFunction, Request, Response, Router } from 'express';
 import { inject, injectable } from 'inversify';
+import Joi from 'joi';
 import { IApiResponse } from '../../common/interface';
 import { IMiddleware } from '../../middleware/interface';
 import { TYPES } from '../../type';
 import { checkRequired } from '../../util';
+import { validateContext } from '../../util/validate';
 import { IHttpRouter } from '../interface';
 import { IPartyService } from './interface';
 
@@ -20,7 +22,11 @@ export default class PartyRouter implements IHttpRouter {
     this.router.get('/search/book', async (request: Request, response: Response, next: NextFunction) => {
       this.apiResponse.generateResponse(request, response, next, async () => {
         const { title, page } = request.query;
-        checkRequired([title]);
+        const schema = Joi.object({
+          title: Joi.string().min(3).max(20).required(),
+          page: Joi.number().required(),
+        });
+        validateContext({ title, page }, schema);
         return await this.partyService.searchBook(String(title), Number(page));
       });
     });
@@ -32,23 +38,38 @@ export default class PartyRouter implements IHttpRouter {
       });
     });
 
+    this.router.get('/participate/:userID', async (request: Request, response: Response, next: NextFunction) => {
+      this.apiResponse.generateResponse(request, response, next, async () => {
+        const { userID } = request.params;
+        const schema = Joi.string().required();
+        validateContext(userID, schema);
+        return await this.partyService.getParticipatePartyList(Number(userID));
+      });
+    });
+
+    this.router.post(
+      '/join',
+      this.middleware.authorization,
+      async (request: Request, response: Response, next: NextFunction) => {
+        this.apiResponse.generateResponse(request, response, next, async () => {
+          const { userID, partyID } = request.body;
+          const schema = Joi.object({
+            userID: Joi.number().required(),
+            partyID: Joi.string().uuid().required(),
+          });
+          validateContext(request.body, schema);
+          return await this.partyService.joinParty(userID, partyID);
+        });
+      }
+    );
+
     // Get relation party
     this.router.get('/relation/:bookID', async (request: Request, response: Response, next: NextFunction) => {
       this.apiResponse.generateResponse(request, response, next, async () => {
         const { bookID } = request.params;
-        checkRequired([bookID]);
+        const schema = Joi.string().required();
+        validateContext(bookID, schema);
         return await this.partyService.getRelationPartyList(bookID);
-      });
-    });
-
-    // Get party detail
-    this.router.get('/:nickname/:URLSlug', async (request: Request, response: Response, next: NextFunction) => {
-      this.apiResponse.generateResponse(request, response, next, async () => {
-        const { nickname, URLSlug } = request.params;
-        const { userID } = request.body;
-        checkRequired([nickname, URLSlug]);
-
-        return await this.partyService.getPartyDetail(nickname, URLSlug, userID);
       });
     });
 
@@ -59,10 +80,48 @@ export default class PartyRouter implements IHttpRouter {
       async (request: Request, response: Response, next: NextFunction) => {
         this.apiResponse.generateResponse(request, response, next, async () => {
           const { party, book, userID, availableDay } = request.body;
-          const { title, memberOfRecruit, isOnline, description } = party;
-          checkRequired([title, memberOfRecruit, availableDay, isOnline, book, description]);
-          // regist party
+          const schema = Joi.object({
+            party: Joi.object({
+              title: Joi.string().min(3).max(20).required(),
+              numberOfRecruit: Joi.number().min(2).max(6),
+              kakaoOpenChatLink: Joi.string().required(),
+              kakaoOpenChatPassword: Joi.string().optional(),
+              region: Joi.string().optional(),
+              city: Joi.string().optional(),
+              town: Joi.string().optional(),
+              isOnline: Joi.boolean().required(),
+              description: Joi.string().required(),
+            }),
+            book: Joi.object({
+              id: Joi.string().required(),
+              title: Joi.string().required(),
+              authors: Joi.array().required(),
+              thumbnail: Joi.string().optional(),
+            }),
+            userID: Joi.number().required(),
+            availableDay: Joi.array().required(),
+          });
+          validateContext(request.body, schema);
           await this.partyService.registParty({ party, book, ownerID: userID, availableDay });
+        });
+      }
+    );
+
+    // Get party detail
+    this.router.get(
+      '/:nickname/:URLSlug',
+      this.middleware.checkLogin,
+      async (request: Request, response: Response, next: NextFunction) => {
+        this.apiResponse.generateResponse(request, response, next, async () => {
+          const { nickname, URLSlug } = request.params;
+          const { userID } = request.body;
+          const schema = Joi.object({
+            nickname: Joi.string().required(),
+            URLSlug: Joi.string().required(),
+          });
+          validateContext(request.params, schema);
+
+          return await this.partyService.getPartyDetail(nickname, URLSlug, userID);
         });
       }
     );
