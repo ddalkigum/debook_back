e9dc39ba2de42621894f76104a3e3af9df679470
ddalkigum <sol35352000@gmail.com>
2022-06-30 06:13:15 +0900

    MODIFY: cors base url

--- src/infrastructure/express/express.ts
+++ src/infrastructure/express/express.ts
@@ -30,7 +30,7 @@ export class ExpressServer implements IServer {
 
   public set = () => {
     this.app.use(helmet());
-    this.app.use(cors({ origin: config.serverConfig.baseURL, credentials: true }));
+    this.app.use(cors({ origin: config.clientConfig.baseURL, credentials: true }));
     this.app.use(cookieParser());
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: false }));
