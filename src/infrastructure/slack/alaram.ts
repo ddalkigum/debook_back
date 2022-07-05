e5aefef488495e092a61c291daab0b5c1b605c67
ddalkigum <sol35352000@gmail.com>
2022-07-06 03:35:17 +0900

    MODIFY: slack alaram message

--- src/infrastructure/slack/alaram.ts
+++ src/infrastructure/slack/alaram.ts
@@ -3,16 +3,21 @@ import { ISlackClient } from './interface';
 import * as config from '../../config';
 import { injectable } from 'inversify';
 import { BaseError } from '../../common/error';
+import { Request } from 'express';
 
 @injectable()
 export default class SlackClient implements ISlackClient {
   private web = new WebClient(config.serverConfig.slackBotToken);
 
-  public sendAlaram = (error, request, response, next) => {
+  public sendAlaram = async (error, request: Request, response, next) => {
     try {
       if (!(error instanceof BaseError)) {
-        this.web.chat.postMessage({
-          text: `Error\n\rname: ${error.name}\n\rmessage: ${error.message}\n\r\`\`\`stack: ${error.stack}\`\`\``,
+        const contextMessage = `context: ${JSON.stringify(request.body)}`;
+        const urlPathMessage = `path: ${request.path}`;
+        const queryMessage = `query: ${JSON.stringify(request.query)}`;
+
+        await this.web.chat.postMessage({
+          text: `Error\n\r${urlPathMessage}\n\r${queryMessage}\n\r${contextMessage}\n\rname: ${error.name}\n\rmessage: ${error.message}\n\r\`\`\`stack: ${error.stack}\`\`\``,
           channel: 'C03MZ1LUJAG',
         });
       }
