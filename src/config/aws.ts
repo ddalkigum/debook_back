b01bef47a2a2eb8b733c7df17f065369206c736b
ddalkigum <sol35352000@gmail.com>
2022-07-07 09:36:04 +0900

    MODIFY: join email address

--- src/config/aws.ts
+++ src/config/aws.ts
@@ -2,8 +2,11 @@ const awsConfig = {
   accessKeyId: process.env.ACCESS_KEY_ID,
   secretAccessKey: process.env.SECRET_ACCESS_KEY,
   region: process.env.REGION,
-  adminEmail: process.env.ADMIN_EMAIL,
   bucketName: process.env.S3_BUCKET_NAME,
+  email: {
+    auth: 'verify@debook.me',
+    noti: 'notify@debook.me',
+  },
 };
 
 export default awsConfig;
