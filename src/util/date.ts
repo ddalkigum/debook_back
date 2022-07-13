e26a2c75b89dc73f5f94a2e283d1fe77aee62e7f
ddalkigum <sol35352000@gmail.com>
2022-07-13 16:50:54 +0900

    MODIFY: certification delete time

--- src/util/date.ts
+++ src/util/date.ts
@@ -1,6 +1,8 @@
 const setDateTime = (deleteTime: number): string => {
-  const timestamp = Date.now() + deleteTime;
-  return new Date().toISOString().slice(0, 19).replace('T', ' ');
+  const timestamp = Date.now() + deleteTime * 1000;
+  const timeDiff = 9 * 60 * 60 * 1000;
+
+  return new Date(timestamp + timeDiff).toISOString().slice(0, 19).replace('T', ' ');
 };
 
 export default { setDateTime };
